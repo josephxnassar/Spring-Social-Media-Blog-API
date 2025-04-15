85ddc5f36ec092291adc149e4a47b88d03dca129
Adam Nassar <ajn619@outlook.com>
2025-04-15 02:56:01 +0000

    Updated Entity Documentation

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -14,13 +14,6 @@ import java.util.Map;
 
 import com.example.exception.UsernameAlreadyExistsException;
 
-/**
- * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
- * found in readme.md as well as the test cases. You be required to use the @GET/POST/PUT/DELETE/etc Mapping annotations
- * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
- */
-
  @RestController
 public class SocialMediaController {
     private final AccountService accountService;
